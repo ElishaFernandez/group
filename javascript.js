53ca54d3d95a4ab7c58a2156fbd57c7b23b72ccf
ElishaFernandez <fernandezelisha19@gmail.com>
2026-03-14 21:18:21 +0800

    updated

---
+++ javascript.js
@@ -0,0 +1,10 @@
+function changep3() {
+    document.getElementById("p3").innerHTML = "Paragraph 3 changed by JavaScript in the EXTERNAL of HTML";
+  }
+  
+function alert3() {
+  alert("This is alert 3 from javascript in the body of HTML");
+}
+function console3() {  
+  console.log("This is console log 3 from javascript in the body of HTML");
+}
